f0b2f2b3ab4c655d388d05e0599967940c32e1b9
BrenoMancini <Breno.manciniss@gmail.com>
2023-08-09 19:47:17 +0100

    Cpp files

--- Source/breno_proj/FL_ToolsForBehaviorTree.cpp
+++ Source/breno_proj/FL_ToolsForBehaviorTree.cpp
@@ -2,4 +2,16 @@
 
 
 #include "FL_ToolsForBehaviorTree.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Camera/CameraComponent.h"
+//Check if Pawn is in Observer sight based on FOV
+bool UFL_ToolsForBehaviorTree::IsOnRadialRange(AActor* Pawn, UCameraComponent* Visualizer, float Fov, float MinFOV, float MaxFOV)
+{
+    FRotator Lookrot = UKismetMathLibrary::FindLookAtRotation(Pawn->GetActorLocation(), Visualizer->GetComponentLocation());
+    FVector LrForward = UKismetMathLibrary::GetForwardVector(Lookrot);
+    FVector VisualizerForward = Visualizer->GetForwardVector();
+    float DotProdResult = FVector::DotProduct(LrForward, VisualizerForward);
+	float NormFOV = UKismetMathLibrary::NormalizeToRange(Fov, MinFOV, MaxFOV);
 
+	return  ((1-NormFOV) + DotProdResult) < 0;// Subtracting 1 by the NormalizedFOV to get cartesian angle difference between Visualizer and Observed
+}
